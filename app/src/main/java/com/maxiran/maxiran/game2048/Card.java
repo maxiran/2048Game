c56e626c8460eceb9032ee22227c2771d0df4d13
maxiran <1661900851@qq.com>
2015-04-14 18:48:28 +0800

    实现游戏逻辑

--- app/src/main/java/com/maxiran/maxiran/game2048/Card.java
+++ app/src/main/java/com/maxiran/maxiran/game2048/Card.java
@@ -32,7 +32,11 @@ public class Card extends FrameLayout {
 
     public void setNum(int num) {
         this.num = num;
-        label.setText(num+"");
+        if(num<=0){
+            label.setText("");
+        }else {
+            label.setText(num+"");
+        }
     }
 
     public int getNum() {
--- app/src/main/java/com/maxiran/maxiran/game2048/GameView.java
+++ app/src/main/java/com/maxiran/maxiran/game2048/GameView.java
@@ -1,9 +1,14 @@
 package com.maxiran.maxiran.game2048;
 import android.content.Context;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 定义整个卡片为GridLayout
  */
@@ -65,34 +70,132 @@ public class GameView extends GridLayout{
 
         int carWidth = (Math.min(w,h)-10)/4;
         addCards(carWidth,carWidth);
+        startGame();
     }
     private void addCards(int cardWidth,int cardHeight){
         Card c;
         for (int y = 0;y <4;y++){
            for (int x = 0;x<4;x++){
                c = new Card(getContext());
-               c.setNum(2);
+               c.setNum(0);
                addView(c,cardWidth,cardHeight);
                cardsMap[x][y] = c;
            }
         }
     }
 
-    private void addRandomNum(){
+    private void startGame(){
 
+        for(int y = 0;y < 4;y++){
+            for(int x = 0;x < 4;x++){
+                cardsMap[x][y].setNum(0);
+            }
+        }
+        addRandomNum();
+        addRandomNum();
+        addRandomNum();
+        addRandomNum();
+        addRandomNum();
+        addRandomNum();
+        addRandomNum();
+    }
+
+    private void addRandomNum(){
+        emptyPoints.clear();
+       for (int y = 0;y<4;y++){
+           for(int x =0;x<4;x++){
+               if(cardsMap[x][y].getNum()<=0){
+                 emptyPoints.add(new Point(x,y));
+               }
+           }
+       }
+        Point p = emptyPoints.remove((int)(Math.random()*emptyPoints.size()));
+        cardsMap[p.x][p.y].setNum(Math.random()>0.1?2:4);
     }
 
     private void swipeLeft(){
-
+       for (int y = 0;y<4;y++){
+           for(int x =0;x <4;x++){
+             for (int x1 = x+1;x1 < 4; x1++){
+                if (cardsMap[x1][y].getNum()>0){//右边有值
+                    if (cardsMap[x][y].getNum()<=0){//右边有值且当前无值，当前赋上右边值
+                       cardsMap[x][y].setNum(cardsMap[x1][y].getNum());
+                       cardsMap[x1][y].setNum(0);
+                        x--;
+                        break;
+                    }else if (cardsMap[x1][y].equals(cardsMap[x][y])){//右边值和当前值相等，相加
+                        cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+                        cardsMap[x1][y].setNum(0);
+                        break;
+                    }
+                }
+             }
+           }
+       }
     }
     private void swipeRight(){
+        for (int y = 0;y<4;y++){
+            for(int x =3;x >=0;x--){
+                for (int x1 = x-1;x1 >=0; x1--){
+                    if (cardsMap[x1][y].getNum()>0){//左边有值
+                        if (cardsMap[x][y].getNum()<=0){//左边有值且当前无值，当前赋上左边值
+                            cardsMap[x][y].setNum(cardsMap[x1][y].getNum());
+                            cardsMap[x1][y].setNum(0);
+                            x++;
+                            break;
+                        }else if (cardsMap[x1][y].equals(cardsMap[x][y])){//右边值和当前值相等，相加
+                            cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+                            cardsMap[x1][y].setNum(0);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
     }
     private void swipeUp(){
+        for (int x = 0;x<4;x++){
+            for(int y =0;y <4;y++){
+                for (int y1 = y+1;y1 < 4; y1++){
+                    if (cardsMap[x][y1].getNum()>0){//右边有值
+                        if (cardsMap[x][y].getNum()<=0){//右边有值且当前无值，当前赋上右边值
+                            cardsMap[x][y].setNum(cardsMap[x][y1].getNum());
+                            cardsMap[x][y1].setNum(0);
+                            y--;
+                            break;
+                        }else if (cardsMap[x][y1].equals(cardsMap[x][y])){//右边值和当前值相等，相加
+                            cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+                            cardsMap[x][y1].setNum(0);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
     }
     private void swipeDown(){
+        for (int x = 0;x<4;x++){
+            for(int y =3;y >=0;y--){
+                for (int y1 = y-1;y1 >= 0; y1--){
+                    if (cardsMap[x][y1].getNum()>0){//右边有值
+                        if (cardsMap[x][y].getNum()<=0){//右边有值且当前无值，当前赋上右边值
+                            cardsMap[x][y].setNum(cardsMap[x][y1].getNum());
+                            cardsMap[x][y1].setNum(0);
+                            y++;
+                            break;
+                        }else if (cardsMap[x][y1].equals(cardsMap[x][y])){//右边值和当前值相等，相加
+                            cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+                            cardsMap[x][y1].setNum(0);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
     }
     private Card[][] cardsMap = new Card[4][4];
-        }
+    private List<Point> emptyPoints = new ArrayList<>();
+ }
