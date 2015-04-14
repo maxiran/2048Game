21d1b865d3fd4f1aab084e8b49d2704894fd1f22
maxiran <1661900851@qq.com>
2015-04-14 13:07:32 +0800

    完成游戏交互设计

--- app/src/main/java/com/maxiran/maxiran/game2048/GameView.java
+++ app/src/main/java/com/maxiran/maxiran/game2048/GameView.java
@@ -1,6 +1,9 @@
 package com.maxiran.maxiran.game2048;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridLayout;
 public class GameView extends GridLayout{
 
@@ -18,8 +21,50 @@ public class GameView extends GridLayout{
         super(context, attrs);
         initGameView();
             }
-    private void initGameView(){
 
+    private void initGameView(){
+        setOnTouchListener(new OnTouchListener(){
+            private float startX,startY,offsetX,offsetY;
+            public boolean onTouch(View v,MotionEvent event){
+               switch (event.getAction()){
+                   case MotionEvent.ACTION_DOWN:
+                       startX = event.getX();
+                       startY = event.getY();
+                       break;
+                   case MotionEvent.ACTION_UP:
+                       offsetX = event.getX()-startX;
+                       offsetY = event.getY()-startY;
+
+                       if (Math.abs(offsetX)>Math.abs(offsetY)){
+                           if (offsetX<-5){
+                              swipeLeft();
+                           }else if (offsetX>5){
+                              swipeRight();
+                           }
+                       }else {
+                           if (offsetY<-5){
+                              swipeUp();
+                           }else if (offsetY>5){
+                              swipeDown();
+                           }
+                       }
+                       break;
+               }
+                return true;
+            }
+        });
     }
 
+    private void swipeLeft(){
+
+    }
+    private void swipeRight(){
+
+    }
+    private void swipeUp(){
+
+    }
+    private void swipeDown(){
+
+    }
         }
