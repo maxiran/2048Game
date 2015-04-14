c5e19d0caa5b13033c0f4ea2b9d8fcf41c7c1742
maxiran <1661900851@qq.com>
2015-04-14 09:43:50 +0800

    First commit

---
+++ app/src/main/java/com/maxiran/maxiran/game2048/MainActivity.java
@@ -0,0 +1,19 @@
+package com.maxiran.maxiran.game2048;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
